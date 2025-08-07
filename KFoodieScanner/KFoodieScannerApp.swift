b7c5cf5add010efed9b2c97c9b2017ac5fccf067
CerinSeo <serin9470@gmail.com>
2025-08-07 11:39:53 +0900

    start

---
+++ KFoodieScanner/KFoodieScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KFoodieScannerApp.swift
+//  KFoodieScanner
+//
+//  Created by 서세린 on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct KFoodieScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
